5f18493fc95faed21a1b4725e59c0b3838c6a65c
Deepjyoti Baishya <deepjyoti.baishya@relyhealthtech.com>
2023-06-23 17:35:48 +0530

    first commit

---
+++ ASP.NET_WebAPI6/Entities/DBContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_WebAPI6.Entities
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost,1433;Database=TutorialDB;User Id=sa;Password=Deep12345678@;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(45);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ASP.NET_WebAPI6/Program.cs
@@ -0,0 +1,62 @@
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using ASP.NET_WebAPI6.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+ 
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEntityFrameworkSqlServer()
+    .AddDbContext<DBContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+builder.Services.AddControllers();
+
+
+ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
+{
+    // Load the trusted CA certificate from file
+    //string trustedCACertificatePath = "path/to/trusted-ca-certificate.crt";
+    string trustedCACertificatePath = "./cert.crt";
+    X509Certificate2 trustedCACertificate = new X509Certificate2(trustedCACertificatePath);
+
+    chain.ChainPolicy.ExtraStore.Add(trustedCACertificate);
+    chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown |
+                                          X509VerificationFlags.IgnoreCtlSignerRevocationUnknown |
+                                          X509VerificationFlags.IgnoreEndRevocationUnknown |
+                                          X509VerificationFlags.IgnoreRootRevocationUnknown;
+
+    bool isValid = chain.Build((X509Certificate2)certificate);
+
+    return isValid;
+};
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
